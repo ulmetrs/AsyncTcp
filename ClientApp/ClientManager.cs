a42d43d8123e28ad47b613f4d8d4037403165ad4
Robert Ulmet <ulmetrs@vt.edu>
2020-01-05 17:47:20 -0500

    solution fixes

---
+++ ClientApp/ClientManager.cs
@@ -0,0 +1,47 @@
+﻿using AsyncTcp;
+using AsyncTest;
+using System;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    public class ClientManager : IAsyncHandler
+    {
+        public AsyncClient AsyncClient { get; }
+        public string ServerHostName { get; }
+
+        public ClientManager(string serverHostName)
+        {
+            ServerHostName = serverHostName;
+            AsyncClient = new AsyncClient(this, 1024, 10);
+        }
+
+        public Task Start()
+        {
+            return AsyncClient.Start(ServerHostName, 9050, true);
+        }
+
+        public void Shutdown()
+        {
+            AsyncClient.Stop();
+        }
+
+        public async Task PeerConnected(AsyncPeer peer)
+        {
+            await Console.Out.WriteLineAsync($"Client (PeerId: {peer.PeerId}) connected...").ConfigureAwait(false);
+        }
+
+        public async Task PeerDisconnected(AsyncPeer peer)
+        {
+            await Console.Out.WriteLineAsync($"Client (PeerId: {peer.PeerId}) disconnected...").ConfigureAwait(false);
+        }
+
+        public async Task DataReceived(AsyncPeer peer, int dataType, byte[] data)
+        {
+            var bytes = await AsyncTcp.Utils.DecompressWithGzipAsync(data).ConfigureAwait(false);
+            var message = Utf8Json.JsonSerializer.Deserialize<TestMessage>(bytes);
+
+            await Console.Out.WriteLineAsync($"Client (PeerId: {peer.PeerId}) Received Message: {message.index}").ConfigureAwait(false);
+        }
+    }
+}
---
+++ ClientApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var scenarios = new Scenarios("192.168.99.1");
+            await scenarios.RunDataScenarioAsync().ConfigureAwait(false);
+
+            await Console.In.ReadLineAsync().ConfigureAwait(false);
+        }
+    }
+}
